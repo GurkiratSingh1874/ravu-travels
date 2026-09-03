f331e551a5b349e73f5a12f443722ec84a15c5c4
GurkiratSingh1874 <gurkiratsingh1874@gmail.com>
2026-09-03 23:57:14 +0530

    build: add prisma generate to build script and force dynamic rendering for production

--- app/(website)/layout.tsx
+++ app/(website)/layout.tsx
@@ -2,6 +2,8 @@ import Navbar from "@/components/website/Navbar";
 import Footer from "@/components/website/Footer";
 import { Toaster } from "react-hot-toast";
 
+export const dynamic = "force-dynamic";
+
 export default function WebsiteLayout({
   children,
 }: {
